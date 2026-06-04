6c1097d9fda33e1b305d32e28adb1e2d4630b3e0
yugasaisrinivasGottumukkala <yugasaisrinivas@gmail.com>
2026-06-04 09:56:28 +0530

    initial commit: add files

---
+++ commentsvar.py
@@ -0,0 +1,11 @@
+print("comments and variables") #print method to print the content to screen
+a=1 #variable a storing integer 1
+b=2 # variable b storing integer 2
+c =a+b # variable c stores sum of a and b
+'''
+ths is the doc string using to write multi line comments
+a=1 and b=2
+so c= 1 + 2 
+C will store 3 
+'''
+print("a + b = ",c)
---
+++ namingconv.py
@@ -0,0 +1,11 @@
+'''Python Variable Naming Conventions 
+    3 Types
+    1 camel Case --- pythonVariable
+    2 Pasal Case --- PythonVariable
+    3 snake case --- python_variable
+    '''
+variableA=1
+VariableB=2
+variable_c= variableA + VariableB
+
+print(variable_c)
